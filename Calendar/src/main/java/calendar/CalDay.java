3e1c9192053c51446249e97f928024b553c9012d
shonkap <32246364+shonkap@users.noreply.github.com>
2018-04-09 23:53:51 -0700

    Update CalDay.java

--- Calendar/src/main/java/calendar/CalDay.java
+++ Calendar/src/main/java/calendar/CalDay.java
@@ -114,7 +114,7 @@ public class CalDay {
 	
 	/** Sets appts */
 	private void setAppts(LinkedList<Appt> appts) {
-		if(appts!=null)
+		if(appts==null)
 				this.appts = appts;
 		
 		if(appts!=null&&appts.size()==0)
