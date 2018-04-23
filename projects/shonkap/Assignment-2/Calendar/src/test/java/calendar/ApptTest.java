a4051a8708e8ea5c33019d5317e4d0f16d5c5c2b
shonkap <shadowgotmilk@gmail.com>
2018-04-23 13:17:05 -0700

    fixing these files

--- projects/shonkap/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/shonkap/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -20,23 +20,23 @@ public class ApptTest  {
   }
   @Test(timeout = 4000)
   public void test01()  throws Throwable  {
-      Appt appt0 = new Appt(6, 45, 2, 20, 2018, "Work", "Time to go to work", "Jonnylong@gmail.com");
-      String string0 = appt0.toString();
-      assertEquals(2, appt0.getRecurBy());
-      assertFalse(appt0.isRecurring());
-      assertEquals("\t20/2/2018 at 6:45am ,Work, Time to go to work\n", string0);
-      assertEquals(0, appt0.getRecurIncrement());
-      appt0.setValid();
+      Appt appt1 = new Appt(6, 45, 2, 20, 2018, "Work", "Time to go to work", "Jonnylong@gmail.com");
+      String string1 = appt1.toString();
+      assertEquals(2, appt1.getRecurBy());
+      assertFalse(appt1.isRecurring());
+      assertEquals("\t20/2/2018 at 6:45am ,Work, Time to go to work\n", string1);
+      assertEquals(0, appt1.getRecurIncrement());
+      appt1.setValid();
   }
   @Test(timeout = 4000)
-  public void test01()  throws Throwable  {
-      Appt appt0 = new Appt(21, 00, 8, 2, 2018, "Date", "Lunch date at mikes", "matt@yahoo.com");
-      String string0 = appt0.toString();
-      assertEquals(2, appt0.getRecurBy());
-      assertFalse(appt0.isRecurring());
-      assertEquals("\t2/8/2018 at 9:00pm ,Date, Lunch date at mikes\n", string0);
-      assertEquals(0, appt0.getRecurIncrement());
-      appt0.setValid();
+  public void test02()  throws Throwable  {
+      Appt appt2 = new Appt(21, 00, 8, 2, 2018, "Date", "Lunch date at mikes", "matt@yahoo.com");
+      String string2 = appt2.toString();
+      assertEquals(2, appt2.getRecurBy());
+      assertFalse(appt2.isRecurring());
+      assertEquals("\t2/8/2018 at 9:00pm ,Date, Lunch date at mikes\n", string2);
+      assertEquals(0, appt2.getRecurIncrement());
+      appt2.setValid();
   }
 
 }
--- projects/shonkap/Assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
+++ projects/shonkap/Assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
@@ -12,13 +12,15 @@ public class CalDayTest{
 
   @Test(timeout = 4000)
   public void test00()  throws Throwable  {
-      Appt appt = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
-      assertTrue(appt.isValid());
+      LinkedList<Appt> appts = new LinkedList<Appt>;
+      CalDay calander = new CalDay();
+      Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+      assertTrue(calander.addAppt(appt0));
   }
   @Test(timeout = 4000)
   public void test01()  throws Throwable  {
-      Appt appt = new Appt();
-      assertFalse(appt.isValid());
+      //Appt appt = new Appt();
+      //assertFalse(appt.isValid());
   }
 
 }
