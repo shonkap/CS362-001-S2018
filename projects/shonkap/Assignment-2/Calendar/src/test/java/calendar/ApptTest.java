b08de50b8dbd58823651e488032ce9f725d62165
shonkap <shadowgotmilk@gmail.com>
2018-04-23 11:57:54 -0700

    Fixing stuff

--- projects/shonkap/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/shonkap/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -18,10 +18,25 @@ public class ApptTest  {
       assertEquals(0, appt0.getRecurIncrement());
       appt0.setValid();
   }
-@Test(timeout = 4000)
- public void test01()  throws Throwable  {
-
-
-}
+  @Test(timeout = 4000)
+  public void test01()  throws Throwable  {
+      Appt appt0 = new Appt(6, 45, 2, 20, 2018, "Work", "Time to go to work", "Jonnylong@gmail.com");
+      String string0 = appt0.toString();
+      assertEquals(2, appt0.getRecurBy());
+      assertFalse(appt0.isRecurring());
+      assertEquals("\t20/2/2018 at 6:45am ,Work, Time to go to work\n", string0);
+      assertEquals(0, appt0.getRecurIncrement());
+      appt0.setValid();
+  }
+  @Test(timeout = 4000)
+  public void test01()  throws Throwable  {
+      Appt appt0 = new Appt(21, 00, 8, 2, 2018, "Date", "Lunch date at mikes", "matt@yahoo.com");
+      String string0 = appt0.toString();
+      assertEquals(2, appt0.getRecurBy());
+      assertFalse(appt0.isRecurring());
+      assertEquals("\t2/8/2018 at 9:00pm ,Date, Lunch date at mikes\n", string0);
+      assertEquals(0, appt0.getRecurIncrement());
+      appt0.setValid();
+  }
 
 }
--- projects/shonkap/Assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
+++ projects/shonkap/Assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
@@ -12,11 +12,13 @@ public class CalDayTest{
 
   @Test(timeout = 4000)
   public void test00()  throws Throwable  {
-
+      Appt appt = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+      assertTrue(appt.isValid());
   }
   @Test(timeout = 4000)
   public void test01()  throws Throwable  {
-
+      Appt appt = new Appt();
+      assertFalse(appt.isValid());
   }
 
 }
